85349d96f0e98fbe089d3ce4a3785ac556ed2e9d
Aswin-NK <aswinnkkumar@gmail.com>
2024-09-28 14:06:33 +0530

    Delete Using Module -3 tier Architect directory

--- Using Module -3 tier Architect/local.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  owners = var.businessdivision
-  enivronment = var.environment
-  name = "${var.environment}-${var.businessdivision}"
-  common_tags ={
-      owners = local.owners
-      enivronment = local.enivronment
-      }
-}
